29a6d8c7650e34136bb516b3df6edbb88dc904ee
Indicasta <indicasta@gmail.com>
2020-04-02 05:30:15 +0200

    from: "springfox-swagger2:2.9.2" to: "springfox-swagger2:3.0.0-SNAPSHOT"

--- src/main/java/com/example/demo/config/Swagger2Config.java
+++ src/main/java/com/example/demo/config/Swagger2Config.java
@@ -1,27 +1,19 @@
 package com.example.demo.config;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import com.google.common.net.HttpHeaders;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.models.auth.In;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class Swagger2Config {
 
 	@Bean
